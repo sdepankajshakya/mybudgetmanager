fbcd95fbf5d00b2906a756be9e96c60dec3ae486
Pankaj Shakya <dev.pankajshakya@gmail.com>
2022-09-19 03:14:57 +0530

    Google Login redirect_uri_mismatch

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -71,7 +71,9 @@ const web = {
   auth_uri: 'https://accounts.google.com/o/oauth2/auth',
   token_uri: 'https://oauth2.googleapis.com/token',
   auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
-  client_secret: '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'
+  client_secret: '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',
+  redirect_uris: ['https://mybudgetmanager.herokuapp.com/'],
+  javascript_origins: ['https://mybudgetmanager.herokuapp.com/'],
 };
 
 FullCalendarModule.registerPlugins([interactionPlugin, dayGridPlugin]);
