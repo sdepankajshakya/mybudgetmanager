d8d2c79b9024824e4d081047ae3c47f88b9a4856
Pankaj Shakya <dev.pankajshakya@gmail.com>
2023-01-15 13:06:02 +0530

    minor fix

--- client/src/app/components/overview/overview.component.ts
+++ client/src/app/components/overview/overview.component.ts
@@ -514,7 +514,7 @@ export class OverviewComponent implements OnInit, AfterViewInit {
         labelFormatter: function () {
           const chart = this as any;
           return (
-            chart.name + ': ' + Highcharts.numberFormat(chart.percentage, 2) + '%'
+            chart.name + ': ' + chart.y + '(' + Highcharts.numberFormat(chart.percentage, 2) + '%' + ')'
           );
         },
       },
