4b930ed2ecc27da84453d98d5de8c8db2f2b7562
Pankaj Shakya <dev.pankajshakya@gmail.com>
2023-04-03 11:42:41 +0530

    removing MONGODB_URI from code

--- db.js
+++ db.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
-const connectionUrl = "mongodb+srv://pankajshakya:thisisapasswordformybudgetmanager@mybudgetmanagercluster.adar9bs.mongodb.net/mybudgetmanager?retryWrites=true&w=majority"
-// const connectionUrl = "mongodb://127.0.0.1:27017/mybudgetmanager"; // local connection url
+const connectionUrl = "mongodb://127.0.0.1:27017/mybudgetmanager"; // local connection url
 
 mongoose.connect(
   process.env.MONGODB_URI || connectionUrl,
