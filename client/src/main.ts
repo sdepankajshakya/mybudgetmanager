8413a0b62ceea111c37a4c06496ab3687a0b1c4f
Pankaj Shakya <pankaj@metadesignsolutions.co.uk>
2025-01-26 21:23:27 +0545

    fix: remote service worker

--- client/src/main.ts
+++ client/src/main.ts
@@ -12,7 +12,7 @@ platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/custom-sw.js')
-    .then(registration => console.log('Custom Service Worker registered!', registration))
-    .catch(error => console.error('Service Worker registration failed:', error));
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    registrations.forEach((registration) => registration.unregister());
+  });
 }
