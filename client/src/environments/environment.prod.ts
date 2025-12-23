71d2a31bb3295a953c918dda98d8fcdffd5de42d
Pankaj Shakya <pankaj@metadesignsolutions.co.uk>
2025-12-23 08:29:53 +0530

    fix: update apibaseurl to render

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://mybudgetmanager.up.railway.app/api',
+  apiBaseUrl: 'https://mybudgetmanager-c2ae.onrender.com/api',
   GOOGLE_CLIENT_ID: '530562955070-2r62masjenjk4oksn7s87a9g1f4aq655.apps.googleusercontent.com'
 };
