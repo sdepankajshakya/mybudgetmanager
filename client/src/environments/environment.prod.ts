6c44b931a47162c3507607cf412112f344a15bc1
Pankaj Shakya <sde.pankajshakya@gmail.com>
2026-01-15 15:06:25 +0530

    build: update production build

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiBaseUrl: 'https://mybudgetmanager-c2ae.onrender.com/api',
-  GOOGLE_CLIENT_ID: '530562955070-2r62masjenjk4oksn7s87a9g1f4aq655.apps.googleusercontent.com'
+  GOOGLE_CLIENT_ID: '450799617616-l0866d8de8r8hdmjmoh67abef5701t1i.apps.googleusercontent.com'
 };
